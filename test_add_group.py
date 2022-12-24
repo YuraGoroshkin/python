9084cd3bac3228cdbdd9ef6da0b18959d1f2ca8d
Yura <gyrra777@gmail.com>
2022-12-24 12:57:04 +0300

    тест с группами полностью работает через pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
 from group import Group
 from application import Application
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.refactor_extract_create_group(Group(name="test", header="test", footer="test"))
-        self.app.logout()
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.refactor_extract_create_group(Group(name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+import pytest
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.refactor_extract_create_group(Group(name="test", header="test", footer="test"))
+    app.logout()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.refactor_extract_create_group(Group(name="", header="", footer=""))
+    app.logout()
