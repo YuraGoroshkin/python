9059adea8bd917709a76f8d6bc8ba55020af32e4
Yura <gyrra777@gmail.com>
2022-12-24 23:01:03 +0300

    Выделен класс для работы с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,9 +9,9 @@ def app(request):
     return fixture
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.refactor_extract_create_group(Group(name="test", header="test", footer="test"))
+    app.group.create(Group(name="test", header="test", footer="test"))
     app.session.logout()
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.refactor_extract_create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
