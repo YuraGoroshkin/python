0539517f1600204f81f5a8e7a9fd208c2edd1217
Yura <gyrra777@gmail.com>
2022-12-25 13:40:49 +0300

    Без общей фикстуры

--- fixture/group.py
+++ fixture/group.py
@@ -2,10 +2,6 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def return_to_groups_page(self):
-        # return to groups page
-        wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -30,3 +26,19 @@ class GroupHelper:
         wd = self.app.wd
         # open groups page
         wd.find_element_by_link_text("groups").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        # init group creation
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        # return to groups page
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
