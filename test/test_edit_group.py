654fe4e89c8626ff93ef7ed6b11f4bed49717a4d
Yura <gyrra777@gmail.com>
2022-12-25 18:07:46 +0300

    Чуть красоты

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,5 +4,5 @@ from model.group import Group
 
 def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="1", header="1", footer="1"))
+    app.group.edit_first_group(Group(name="+Edit_name", header="+Edit_header", footer="+Edit_footer"))
     app.session.logout()
