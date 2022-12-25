818eff8edc7181f60ef45f32e654f744a4c6cf7a
Yura <gyrra777@gmail.com>
2022-12-25 13:46:13 +0300

    С общей фикстурой - но без настройки, браузер постоянно запускается

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_contact_empty(app):
@@ -16,8 +7,10 @@ def test_contact_empty(app):
     app.session.login(username="admin", password="secret")
     app.contact.select_add_new()
     app.contact.add(
-        Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", TelephoneHome="",
-                TelephoneMobile="", TelephoneWork="", TelephoneFax="", email="", email2="", email3="", homepage="", byear="", ayear="",
+        Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
+                TelephoneHome="",
+                TelephoneMobile="", TelephoneWork="", TelephoneFax="", email="", email2="", email3="", homepage="",
+                byear="", ayear="",
                 address2="", phone2="", notes=""))
     app.select_home()
     app.session.logout()
@@ -29,7 +22,8 @@ def test_contact(app):
     app.contact.select_add_new()
     app.contact.add(
         Contact(firstname="Yura", middlename="middlename", lastname="Goroshkin", nickname="nickname", title="Title",
-                company="Company", address="Address", TelephoneHome="88005553225", TelephoneMobile="88005553225", TelephoneWork="88005553225",
+                company="Company", address="Address", TelephoneHome="88005553225", TelephoneMobile="88005553225",
+                TelephoneWork="88005553225",
                 TelephoneFax="88005553225", email="test@email.ru", email2="email2", email3="test@gemail.com",
                 homepage="Homepage", byear="1988", ayear="1998", address2="Addres2", phone2="Phone2", notes="Notes"))
     app.select_home()
