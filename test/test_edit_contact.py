878d6bb7d9f85ca0c061030e3a69db1f88e8f86c
Yura <gyrra777@gmail.com>
2022-12-31 16:49:34 +0300

    предусловие для contacts

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -4,6 +4,15 @@ from model.contact import Contact
 
 def test_edit_first_contact(app):
     app.select_home()
+    if app.contact.count() == 0:
+        app.contact.select_add_new()
+        app.contact.add(
+            Contact(firstname="Yura", middlename="middlename", lastname="Goroshkin", nickname="nickname", title="Title",
+                    company="Company", address="Address", telephone_home="88005553225", telephone_mobile="88005553225",
+                    telephone_work="88005553225",
+                    telephone_fax="88005553225", email="test@email.ru", email2="email2", email3="test@gemail.com",
+                    homepage="Homepage", byear="1988", ayear="1998", address2="Addres2", phone2="Phone2", notes="Notes"))
+        app.open_home_page()
     app.contact.edit_first_contact(
         Contact(firstname="correct", middlename="correct", lastname="correct", nickname="correct", title="correct",
                 company="correct", address="correct",
