0f8f8179cbf66de363674a81d2c4259baf0259c8
Yura <gyrra777@gmail.com>
2022-12-31 15:51:44 +0300

    предусловие для edit_group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,8 +3,12 @@ from model.group import Group
 
 
 def test_edit_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='for_edit'))
     app.group.edit_first_group(Group(name="+Edit_name"))
 
 
 def test_edit_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='for_edit'))
     app.group.edit_first_group(Group(header="Header_edit"))
