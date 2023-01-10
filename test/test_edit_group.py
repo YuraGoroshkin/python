46e0e213c3857c46bd431dd462a587b73fa68279
Yura <gyrra777@gmail.com>
2023-01-10 20:48:14 +0300

    Сортировка списков перед сравнением

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,17 +4,21 @@ from model.group import Group
 
 def test_edit_name(app):
     old_groups = app.group.get_group_list()
+    group = Group(name="+Edit_name")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name='for_edit'))
-    app.group.edit_first_group(Group(name="+Edit_name"))
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_edit_header(app):
-    old_groups = app.group.get_group_list()
-    if app.group.count() == 0:
-        app.group.create(Group(name='for_edit'))
-    app.group.edit_first_group(Group(header="Header_edit"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_edit_header(app):
+#     old_groups = app.group.get_group_list()
+#     if app.group.count() == 0:
+#         app.group.create(Group(name='for_edit'))
+#     app.group.edit_first_group(Group(header="Header_edit"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
