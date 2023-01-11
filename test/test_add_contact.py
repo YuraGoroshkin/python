17d47bed253028968b65f26ce4e9815baa6775a3
Yura <gyrra777@gmail.com>
2023-01-11 23:21:12 +0300

    Что-то идёт не так при проверки id в контактах add и edit

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,26 +5,34 @@ from model.contact import Contact
 def test_contact_empty(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.open_add_new()
-    app.contact.add(
-        Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
-                telephone_home="",
-                telephone_mobile="", telephone_work="", telephone_fax="", email="", email2="", email3="", homepage="",
-                byear="", ayear="",
-                address2="", phone2="", notes=""))
+    contact = Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
+                      telephone_home="",
+                      telephone_mobile="", telephone_work="", telephone_fax="", email="", email2="", email3="",
+                      homepage="",
+                      byear="", ayear="",
+                      address2="", phone2="", notes="")
+    app.contact.add(contact)
     app.select_home()
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_contact(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.open_add_new()
-    app.contact.add(
-        Contact(firstname="Yura", middlename="middlename", lastname="Goroshkin", nickname="nickname", title="Title",
-                company="Company", address="Address", telephone_home="88005553225", telephone_mobile="88005553225",
-                telephone_work="88005553225",
-                telephone_fax="88005553225", email="test@email.ru", email2="email2", email3="test@gemail.com",
-                homepage="Homepage", byear="1988", ayear="1998", address2="Addres2", phone2="Phone2", notes="Notes"))
+    contact = Contact(firstname="Yura", middlename="middlename", lastname="Goroshkin", nickname="nickname",
+                      title="Title",
+                      company="Company", address="Address", telephone_home="88005553225",
+                      telephone_mobile="88005553225",
+                      telephone_work="88005553225",
+                      telephone_fax="88005553225", email="test@email.ru", email2="email2", email3="test@gemail.com",
+                      homepage="Homepage", byear="1988", ayear="1998", address2="Addres2", phone2="Phone2",
+                      notes="Notes")
+    app.contact.add(contact)
     app.select_home()
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import time
+
 
 def test_edit_first_contact(app):
     app.select_home()
     old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="correct33334")
+    contact.id = old_contacts[0].id
     if app.contact.count() == 0:
         app.contact.open_add_new()
         app.contact.add(
@@ -12,10 +14,12 @@ def test_edit_first_contact(app):
                     company="Company", address="Address", telephone_home="88005553225", telephone_mobile="88005553225",
                     telephone_work="88005553225",
                     telephone_fax="88005553225", email="test@email.ru", email2="email2", email3="test@gemail.com",
-                    homepage="Homepage", byear="1988", ayear="1998", address2="Addres2", phone2="Phone2", notes="Notes"))
+                    homepage="Homepage", byear="1988", ayear="1998", address2="Addres2", phone2="Phone2",
+                    notes="Notes"))
         app.open_home_page()
-    app.contact.edit_first_contact(
-        Contact(firstname="correct33334"))
+    app.contact.edit_first_contact(contact)
     app.select_home()
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
