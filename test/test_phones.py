59244a90495543e4694c14a0e40883c88762361a
Yura <gyrra777@gmail.com>
2023-01-21 14:02:33 +0300

    Задание №14: Реализовать проверки для всех полей контакта на главной странице

--- test/test_phones.py
+++ test/test_phones.py
@@ -8,12 +8,10 @@ def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    # assert contact_from_home_page.email == merge_email_like_on_home_page(contact_from_edit_page)
-    # assert contact_from_home_page.address == merge_address_like_on_home_page(contact_from_edit_page)
-    d = contact_from_home_page.email
-    print(d + '\n')
-    e = contact_from_home_page.address
-    print(e)
+    assert contact_from_home_page.email == merge_email_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
 
 
 def test_phones_on_contact_view_page(app):
@@ -34,3 +32,10 @@ def merge_phones_like_on_home_page(contact):
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.home, contact.work, contact.mobile, contact.phone2]))))
+
+
+def merge_email_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.email2, contact.email3]))))
