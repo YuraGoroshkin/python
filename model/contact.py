652bb7e4454eda721e00e4c61b2ee74696e7b7bc
Yura <gyrra777@gmail.com>
2023-01-22 14:22:38 +0300

    Параметризация тестов. контакты и группы

--- model/contact.py
+++ model/contact.py
@@ -30,10 +30,15 @@ class Contact:
         self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s" % \
+            (self.id, self.firstname, self.lastname, self.middlename, self.nickname, self.title, self.company,
+             self.address, self.home, self.mobile, self.work, self.fax, self.email, self.email2,
+             self.email3, self.homepage, self.byear, self.ayear, self.address2,
+             self.phone2, self.notes)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
+        return (
+                    self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:
