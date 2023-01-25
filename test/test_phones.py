901b451d8add90861f08f02d7a10509231d84d60
Yura <gyrra777@gmail.com>
2023-01-25 21:24:13 +0300

    Работа над ошибками 2 https://software-testing.ru/lms/mod/assign/view.php?id=290568

--- test/test_phones.py
+++ test/test_phones.py
@@ -8,7 +8,7 @@ def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert clear(contact_from_home_page.email) == merge_email_like_on_home_page(contact_from_edit_page)
+    assert clear_email(contact_from_home_page.email) == clear_email(merge_email_like_on_home_page(contact_from_edit_page))
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
@@ -27,6 +27,10 @@ def clear(s):
     return re.sub("[()  -]", "", s)
 
 
+def clear_email(s):
+    return re.sub("[  ]", "", s)
+
+
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
@@ -36,6 +40,5 @@ def merge_phones_like_on_home_page(contact):
 
 def merge_email_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3]))))
+                            filter(lambda x: x is not None,
+                                   [contact.email, contact.email2, contact.email3])))
