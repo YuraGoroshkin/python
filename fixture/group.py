349e830e7f018839c08de310a708bad11f350a26
Yura <gyrra777@gmail.com>
2023-02-02 19:59:03 +0300

    Отключаемые проверки пользовательского интерфейса

--- fixture/group.py
+++ fixture/group.py
@@ -33,6 +33,10 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
     def delete_group_by_index(self, index):
         wd = self.app.wd
         # init group creation
@@ -42,6 +46,17 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        # init group creation
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
 
     def edit_first_group(self):
         self.edit_group_by_index(0)
