a802efe2b8d26287b0212d567eee358dfc120860
Yura <gyrra777@gmail.com>
2023-02-08 22:42:29 +0300

    Задание №20 попытка #1

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,9 +3,9 @@ from model.contact import Contact
 from random import randrange
 
 
-def test_edit_some_contact(app):
+def test_edit_some_contact(app, db, check_ui):
     app.select_home()
-    if app.contact.count() == 0:
+    if len(db.get_contact_list()) == 0:
         app.contact.open_add_new()
         app.contact.add(
             Contact(firstname="Yura", middlename="middlename", lastname="Goroshkin", nickname="nickname", title="Title",
@@ -15,14 +15,14 @@ def test_edit_some_contact(app):
                     homepage="Homepage", byear="1988", ayear="1998", address2="Addres2", phone2="Phone2",
                     notes="Notes"))
         app.open_home_page()
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     contact = Contact(firstname="correct33334", lastname="Test")
     contact.id = old_contacts[index].id
-    print(contact.id)
     app.contact.edit_contact_by_index(index, contact)
     app.select_home()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) == app.contact.count()
     old_contacts[index] = contact
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,23 +3,17 @@ from model.group import Group
 from random import randrange
 
 
-def test_edit_name(app):
-    if app.group.count() == 0:
+def test_edit_name(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name='for_delete'))
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     index = randrange(len(old_groups))
     group = Group(name="+Edit_name")
     group.id = old_groups[index].id
     app.group.edit_group_by_index(index, group)
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
     old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
-# def test_edit_header(app):
-#     old_groups = app.group.get_group_list()
-#     if app.group.count() == 0:
-#         app.group.create(Group(name='for_edit'))
-#     app.group.edit_first_group(Group(header="Header_edit"))
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
