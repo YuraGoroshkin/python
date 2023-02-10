074184b995b7b29d1dbc8123dbcc2666bfeb54f9
Yura <gyrra777@gmail.com>
2023-02-10 17:18:00 +0300

    Задание №20 попытка #2

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,7 +2,7 @@
 from model.contact import Contact
 
 
-def test_contact(app, db, json_contacts):
+def test_contact(app, db, json_contacts, check_ui):
     contact = json_contacts
     old_contacts = db.get_contact_list()
     app.contact.open_add_new()
@@ -10,4 +10,6 @@ def test_contact(app, db, json_contacts):
     app.select_home()
     new_contacts = db.get_contact_list()
     old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    assert len(old_contacts) == len(new_contacts)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,7 +4,7 @@ import time
 from random import randrange
 
 
-def test_delete_some_contact(app, db):
+def test_delete_some_contact(app, db, check_ui):
     app.select_home()
     if len(db.get_contact_list()) == 0:
         app.contact.open_add_new()
@@ -24,4 +24,6 @@ def test_delete_some_contact(app, db):
     time.sleep(3)
     new_contacts = db.get_contact_list()
     old_contacts.remove(contact)
-    assert old_contacts == new_contacts
+    assert len(old_contacts) == len(new_contacts)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
